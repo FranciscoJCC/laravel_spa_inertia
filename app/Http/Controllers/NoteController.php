da135992e866bd05c8cba7b6f0738c7a01562f4e
FranciscoJCC <fjccandelario@gmail.com>
2022-06-05 22:41:06 -0500

    Se agrega vista de notas con inertia

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
 use App\Models\Note;
 use App\Http\Requests\StoreNoteRequest;
 use App\Http\Requests\UpdateNoteRequest;
@@ -15,7 +16,9 @@ class NoteController extends Controller
      */
     public function index()
     {
-        //
+        return Inertia::render('Notes/Index',[
+            'notes' => Note::latest()->get()
+        ]);
     }
 
     /**
@@ -47,7 +50,7 @@ class NoteController extends Controller
      */
     public function show(Note $note)
     {
-        //
+        return Inertia::render('Notes/Show', compact('note'));
     }
 
     /**
